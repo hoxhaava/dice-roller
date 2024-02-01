6c784c34976f8460296ff6effc0a9d624bb75b75
hoxhahava <hoxha.ava.mak@gmail.com>
2024-02-01 22:00:10 +0100

    Update styling

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,28 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MaterialApp(home: Text("Hava's First Flutter App")));
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              colors: [
+                Color.fromARGB(255, 26, 2, 80),
+                Color.fromARGB(255, 45, 7, 98)
+              ],
+            ),
+          ),
+          child: const Center(
+            child: Text(
+              "Hava's First Flutter App",
+              style: TextStyle(color: Colors.white, fontSize: 28),
+            ),
+          ),
+        ),
+      ),
+    ),
+  );
 }

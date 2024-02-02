23ac782654a3ab9a6468029489a1d3ca788c3326
hoxhahava <hoxha.ava.mak@gmail.com>
2024-02-02 17:45:56 +0100

    Create custom widgets

---
+++ lib/gradient_container.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:first_app/styled_text.dart';
+
+var startAlignment = Alignment.topLeft;
+var endAlignment = Alignment.bottomRight;
+
+class GradientContainer extends StatelessWidget {
+  const GradientContainer({super.key});
+
+  @override
+  Widget build(context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: const [
+            Color.fromARGB(255, 26, 2, 80),
+            Color.fromARGB(255, 45, 7, 98)
+          ],
+          begin: startAlignment,
+          end: endAlignment,
+          
+        ),
+      ),
+      child: const Center(child: StyledText()),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:first_app/gradient_container.dart';
 
 void main() {
   runApp(
-    MaterialApp(
+    const MaterialApp(
       home: Scaffold(
-        body: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-              colors: [
-                Color.fromARGB(255, 26, 2, 80),
-                Color.fromARGB(255, 45, 7, 98)
-              ],
-            ),
-          ),
-          child: const Center(
-            child: Text(
-              "Hava's First Flutter App",
-              style: TextStyle(color: Colors.white, fontSize: 28),
-            ),
-          ),
-        ),
+        body: GradientContainer(),
       ),
     ),
   );

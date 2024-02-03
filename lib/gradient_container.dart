8e0bccdfd38602442b3a11b461461f5e75d8f45d
hoxhahava <hoxha.ava.mak@gmail.com>
2024-02-03 14:03:07 +0100

    Use stateful widget

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,5 +1,5 @@
+import 'package:first_app/dice_roller.dart';
 import 'package:flutter/material.dart';
-import 'package:first_app/styled_text.dart';
 
 const startAlignment = Alignment.topLeft;
 const endAlignment = Alignment.bottomRight;
@@ -19,8 +19,8 @@ class GradientContainer extends StatelessWidget {
           end: endAlignment,
         ),
       ),
-      child: Center(
-        child: Image.asset('assets/images/dice-1.png', width: 200,),
+      child: const Center(
+        child: DiceRoller(),
       ),
     );
   }
